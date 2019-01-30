8b2b29404173ef743a37ac19ea8cf9f118c49475
Cillian Wing <cwing@zagmail.gonzaga.edu>
2019-01-30 11:43:26 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,8 +1,16 @@
 def get_first_name_of_season_winner(data, season_number)
   # code here
+  winner_array = []
+  winner_string = ""
   data.each do |season, info|
     if season == season_number 
-      if info[:]
+      if info["status"] == "Winner"
+        winner_string = info["name"]
+      end
+    end
+  end
+  winner_array = winner_string.split
+  winner_array[0]
 end
 
 def get_contestant_name(data, occupation)
